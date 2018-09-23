206329f4ea40e75a9a4ac8a2469b43333a346dba
Night <1340legia@gmail.com>
2018-09-23 11:19:02 +1000

    HTTP
    HTTP

---
+++ app/src/main/java/com/example/macintosh/assignmentt1/HTTP/AbstractHttpAsyncTask.java
@@ -0,0 +1,60 @@
+package com.example.macintosh.assignmentt1.HTTP;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.example.macintosh.assignmentt1.Activities.MainActivity;
+
+
+
+// example of HttpURLConnection by Caspar, updated sem 2, 2018
+public abstract class AbstractHttpAsyncTask extends AsyncTask<Void, Integer, Void>
+{
+   private static final String LOG_TAG = AbstractHttpAsyncTask.class.getName();
+
+   protected StringBuilder htmlStringBuilder = new StringBuilder();
+
+   // this one has a valid CONTENT_LENGTH header
+   public static final String TEST_URL = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=-37.814644,144.955412&destinations=-37.810045,144.964220&key=AIzaSyCDbBGQ8CaRLa4rvOhsaG-LO0Rxy0CUGxI";
+
+   // this one does not always set the content length so makes progress tracking
+   // difficult
+   // static final String TEST_URL = "http://www.wikipedia.org/";
+
+   protected MainActivity activity = null;
+   private int charsRead;
+
+   public AbstractHttpAsyncTask(MainActivity activity)
+   {
+      this.activity = activity;
+   }
+
+   @Override
+   protected void onProgressUpdate(Integer... progress)
+   {
+      if (activity == null)
+         Log.w(LOG_TAG, "onProgressUpdate() skipped -- no activity");
+      else
+      {
+         // Log.i(LOG_TAG, "Task progress=" + progress[0] + "%");
+         activity.updateProgress(progress[0]);
+      }
+   }
+
+   protected void doProgress(int charsRead, int length)
+   {
+      this.charsRead += charsRead;
+      // delay allows us to see progress on fast network!
+      //Thread.sleep(1);
+      // convert to percentage for progress update (standard 0..100 range)
+      int progress = (int) ((double) this.charsRead / length * 100.0);
+      Log.i(LOG_TAG, Integer.toString(progress) + "%");
+      publishProgress(progress);
+   }
+
+   @Override
+   protected void onPostExecute(Void result)
+   {
+      activity.displayHTML(htmlStringBuilder.toString());
+   }
+}
---
+++ app/src/main/java/com/example/macintosh/assignmentt1/HTTP/HttpClientApacheAsyncTask.java
@@ -0,0 +1,105 @@
+package com.example.macintosh.assignmentt1.HTTP;
+
+import android.util.Log;
+
+import com.example.macintosh.assignmentt1.Activities.MainActivity;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+
+// example of HttpURLConnection by Caspar, updates sem 2, 2018
+public class HttpClientApacheAsyncTask extends AbstractHttpAsyncTask
+{
+   private String LOG_TAG = HttpClientApacheAsyncTask.class.getName();
+
+   public HttpClientApacheAsyncTask(MainActivity activity)
+   {
+      super(activity);
+   }
+
+   @Override
+   // Runs in the worker thread
+   protected Void doInBackground(Void... unused)
+   {
+      HttpClient httpclient = new DefaultHttpClient();
+      HttpGet getRequest = new HttpGet(TEST_URL);
+
+      try
+      {
+         Log.i(LOG_TAG, "starting");
+         // the easy way using BasicResponseHandler
+         String responseBody = httpclient.execute(getRequest,
+                 new BasicResponseHandler());
+         // log the full HTML
+         Log.i(LOG_TAG, responseBody);
+
+         Log.i(LOG_TAG, "MANUAL with progress:");
+
+         // the manual way retrieving a content entity
+         HttpResponse response = httpclient.execute(getRequest);
+         HttpEntity entity = response.getEntity();
+         logHeaders(response.getAllHeaders());
+
+         if (entity != null)
+         {
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    entity.getContent()));
+
+            // for progress updates
+            int length = (int) entity.getContentLength();
+
+            // some sites don't always set the length so we guess!
+            // OPTION: could switch to indeterminate progress widget and display
+            // ongoing data volume downloaded
+            if (length == -1)
+               length = 11000;
+
+            Log.i(LOG_TAG, "getContentLength()=" + length);
+
+            String line;
+            // read chunk at a time so we can publish progress
+            while ((line = br.readLine()) != null)
+            {
+               // log individual line
+               //Log.i(LOG_TAG, line);
+               htmlStringBuilder.append(line);
+               doProgress(line.length(), length);
+            }
+            // finished
+            publishProgress(100);
+            // Log.i(LOG_TAG, htmlStringBuilder.toString());
+            Log.i(LOG_TAG, "DONE");
+
+            // UNCOMMENT following if you want to display the responseBody which holds the Entity
+            // content instead
+            // htmlStringBuilder=new StringBuilder();
+            // htmlStringBuilder.append(responseBody);
+         }
+      }
+      catch (Exception e)
+      {
+         e.printStackTrace();
+      }
+      return null;
+   }
+
+   private void logHeaders(Header[] headers)
+   {
+      StringBuffer sb = new StringBuffer();
+      // since already using a StringBuffer will not use String.format() here
+      for (Header header : headers)
+         sb.append("Header: ").append(header.getName()).append(", Value: ").append(header.getValue())
+                 .append('\n');
+      Log.i(LOG_TAG, sb.toString());
+   }
+}
