8863981ab0047828412f5c9c733cbb2626e8db6c
vinhngogia0906 <vinhngogia0906@gmail.com>
2018-09-23 22:10:12 +1000

    merger

--- app/src/main/java/com/example/macintosh/assignmentt1/Activities/MainActivity.java
+++ app/src/main/java/com/example/macintosh/assignmentt1/Activities/MainActivity.java
@@ -50,8 +50,8 @@ public class MainActivity extends AppCompatActivity  {
         TrackingService trackingService = new TrackingService();
         trackingService.parseFile(getApplicationContext());
         final String db = "jdbc:sqldroid:" + getDatabasePath("ass1.db").getAbsolutePath();
-        JDBCActivity jdbcActivity = new JDBCActivity();
-        jdbcActivity.trackingDataDatabase(getApplicationContext(),db);
+//        JDBCActivity jdbcActivity = new JDBCActivity();
+//        jdbcActivity.trackingDataDatabase(getApplicationContext(),db);
         dataa = new ArrayList<>();
         trackingData = new ArrayList<>();
         recyclerView = findViewById(R.id.recycler_view);
